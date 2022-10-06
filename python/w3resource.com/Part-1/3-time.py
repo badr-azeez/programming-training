28ad13a7b8483995a593ef37385d25b18102dbbe
Badr Azeez <badr@example.com>
2022-10-06 23:54:25 +0000

    add time.py file

---
+++ python/w3resource.com/Part-1/3-time.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from datetime import datetime
+print('Current data and time')
+year = datetime.now().year
+month = datetime.now().month
+day = datetime.now().day
+hour = datetime.now().hour
+minute = datetime.now().minute
+second = datetime.now().second
+print( year, '-', month, '-', day,'-', ' ',hour,':', minute,':', second,sep='')
+
+print('-' * 9)
+now = datetime.now()
+print(now.strftime('%Y-%m-%d %H:%M:%S'))
